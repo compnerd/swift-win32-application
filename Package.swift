d3e5306b207fd6c7cec92a629f4074d31475fe7c
Saleem Abdulrasool <compnerd@compnerd.org>
2021-05-30 11:39:13 -0700

    build: improve GUI application handling for Swift 5.4
    The `-entry-point-function-name` option is not available prior to Swift
    5.5.  Use an alternative mechanism to expose a `wWinMain` and select the
    GUI entry point.

--- Package.swift
+++ Package.swift
@@ -2,6 +2,29 @@
 
 import PackageDescription
 
+#if swift(>=5.5)
+let GUISwiftSettings: [SwiftSetting] = [
+  // Instruct the compiler to generate the entry point as `wWinMain`
+  // rather than `main` to ensure that the application is correctly
+  // marked as a GUI application (Windows subsystem).  This is done
+  // by the linker based upon the main entry point.  Simply marking
+  // the subsystem is insufficient as the compiler will materialize
+  // the entry point as `main`, resulting in an undefined symbol.
+  .unsafeFlags([
+    "-Xfrontend", "-entry-point-function-name",
+    "-Xfrontend", "wWinMain",
+  ]),
+]
+let GUILinkerSettings: [LinkerSetting] = [
+]
+#else
+let GUISwiftSettings: [SwiftSetting] = [
+]
+let GUILinkerSettings: [LinkerSetting] = [
+  .unsafeFlags(["-Xlinker", "/SUBSYSTEM:WINDOWS"]),
+]
+#endif
+
 let Application = Package(
   name: "Application",
   products: [
@@ -21,16 +44,8 @@ let Application = Package(
         "Application.exe.manifest",
         "Info.plist",
       ],
-      swiftSettings: [
-        // Instruct the compiler to generate the entry point as `wWinMain`
-        // rather than `main` to ensure that the application is correctly
-        // marked as a GUI application (Windows subsystem).  This is done
-        // by the linker based upon the main entry point.  Simply marking
-        // the subsystem is insufficient as the compiler will materialize
-        // the entry point as `main`, resulting in an undefined symbol.
-        .unsafeFlags(["-Xfrontend", "-entry-point-function-name",
-                      "-Xfrontend", "wWinMain"]),
-      ]
+      swiftSettings: GUISwiftSettings,
+      linkerSettings: GUILinkerSettings
     ),
     .testTarget(name: "ApplicationTests", dependencies: ["Application"]),
   ]
--- Sources/Application/AppDelegate.swift
+++ Sources/Application/AppDelegate.swift
@@ -2,7 +2,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import SwiftWin32
+#if swift(<5.5)
+import WinSDK
+#endif
 
 @main
 final class Application: ApplicationDelegate {
 }
+
+#if swift(<5.5)
+@_silgen_name("wWinMain")
+func wWinMain(_ hInstance: HINSTANCE, _ hPrevInstance: HINSTANCE,
+              _ pCmdLine: PWSTR, _ nCmdShow: CInt) -> CInt {
+  return ApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, nil)
+}
+#endif
